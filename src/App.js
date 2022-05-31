7555fb773e48e8bf93fe98f80f0c6475885995ff
mar-tchelo <marcelo.js10@gmail.com>
2022-05-31 14:30:05 -0300

    first commit HTML and CSS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import './app.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  return(
+   <div className="app">
+      <h1 >calculadora IMC</h1>
+      <span>Vamos calcular seu imc</span>
+
+      <div className="area-input">
+        <input type="text"
+        placeholder="Peso em (kg) Ex: 90"
+        />
+        <input type="text"
+        placeholder="Altura em (cm) Ex: 180"
+        />
+        <button>Cacular</button>
+        <h2>Seu imc foi 25, você esta abaixo do peso!</h2>
+      </div>
+
+  </div>
+  )
 }
 
-export default App;
